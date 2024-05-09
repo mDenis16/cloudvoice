3dac268d379cce6500779e31b8a36fcc8c5cb43a
mDenis16 <topordenis164@gmail.com>
2024-05-09 22:57:07 +0300

    initial file upload

---
+++ cloud-voice-next/src/app/page.tsx
@@ -0,0 +1,26 @@
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { PrismaClient } from "@prisma/client";
+import Image from "next/image";
+
+export default async function Home() {
+  let cloudflare_db = getRequestContext().env.DB;
+
+  const adapter = new PrismaD1(cloudflare_db);
+  const prisma = new PrismaClient({ adapter });
+
+  const users = await prisma.user.findMany();
+  const result = JSON.stringify(users);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/app/page.tsx</code>
+        </p>
+        <p>mata {result}</p>
+      </div>
+    </main>
+  );
+}
+export const runtime = 'edge'
